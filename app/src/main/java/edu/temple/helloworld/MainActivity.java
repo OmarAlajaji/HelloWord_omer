b3dc4d2e793659de24a1d6c8b147482ef7f3691f
AmmanKhan <ammankhaniet1@gmail.com>
2020-09-17 18:33:11 +0500

    Added Button to update text

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView)findViewById(R.id.textView);
+        Button btnClick = (Button)findViewById(R.id.btnClick);
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(getResources().getString(R.string.you_have_click_the_button));
+            }
+        });
     }
 }
